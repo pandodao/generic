d651b854d4d20f156f9aa1bbcc507d8f17f7e07c
yiplee <guoyinl@gmail.com>
2023-09-12 17:04:13 +0800

    feat: add reverse function

--- functions.go
+++ functions.go
@@ -18,3 +18,10 @@ func Try[T any](v T, err error) T {
 func Ptr[T any](v T) *T {
 	return &v
 }
+
+// Reverse reverses the elements of s.
+func Reverse[T any](s []T) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
